833e26a6ca4c6be41db25b845402aa879a84ed77
Joel Ferreira <j.prata14@hotmail.com>
2018-08-20 20:07:11 +0100

    Correction to the logic used to choose a ball color. Previously the balls were given a color based on their generation order and not based on the random number given to them.

--- src/ballGenerator.js
+++ src/ballGenerator.js
@@ -11,20 +11,21 @@ function BallGenerator() {
 
 
     /**
-    * Creates the balls for the ball game.
+    * Creates the balls for the ball game, giving them random numbers between 1 and 60 and a corresponding color image.
     * @param {number} number Total number of balls. The balls generated will obey the color restrictions established for this generator.
     * @param {Array<string>} colorsToUse List of colors to use in the animation. The columns of balls will have colors corresponding to each one named in this list.
-    * @param {number} colorInterval The number of balls of a particular color that should be created before using the next color.
+    * @param {number} colorInterval The number of balls of a particular color that should be created before using the next color, depending on the random number attributed to the ball.
     * @param {Function} callback The callback function that returns the list of balls created. Invoked as callback(balls) where balls is a list of Ball objects.
     */
     this.createBalls = function (number, colorsToUse, colorInterval, ballID, callback) {
-        if (number >= 1 && number <= 60) {
+        if (number >= 1 && number <= 60 && colorInterval < 60) {
             let balls = [];
             let ballId = ballID;
             let idNumber = 1;
             //list of random numbers already used
             let usedNumbers = [];
             let ball;
+
             for (let index = 0; index < number; index++) {
                 //generating random number until a new one is found
                 let ballNumber = Math.floor((Math.random() * 60) + 1);
@@ -37,17 +38,40 @@ function BallGenerator() {
                 idNumber++;
             }
 
-            //adding the correct colored ball to the object according to the color interval specified
-            let colorCounter = 0;
-            let colorIndex = 0;
+            //create a list of checkpoints in the random numbers between 1 and 60 where the color of a ball with a number out of the previous ones' changes
+            let numberCheckPoints = [];
+            let auxPoint = colorInterval;
+            numberCheckPoints.push(0);
+            numberCheckPoints.push(auxPoint);
+            while(auxPoint+colorInterval <= 60){
+                auxPoint+=colorInterval;
+                numberCheckPoints.push(auxPoint);
+            }
+            let lastCheckPoint = numberCheckPoints.pop();
+            if(lastCheckPoint < 60){
+                numberCheckPoints.push(lastCheckPoint);
+                numberCheckPoints.push(60);
+            }else{
+                numberCheckPoints.push(lastCheckPoint);
+            }
+
+            //adding the correct colored ball to the object according to the color interval specified and the random number
             for (let index = 0; index < balls.length; index++) {
                 let element = balls[index];
-                element.image = colors[colorsToUse[colorIndex]];
-                colorCounter++;
-                if (colorCounter > colorInterval - 1) {
-                    colorCounter = 0;
-                    colorIndex++;
-                    if (colorIndex > colorsToUse.length - 1) colorIndex = 0;
+                let colorIndex = 0;
+                //check if the ball number falls in any of the number intervals and use the corresponding color
+                //the color is chosen from the sequence provided in colorsToUse. The sequence repeats itself as many times as possible within the 60 available numbers.
+                for (let i = 1; i < numberCheckPoints.length; i++) {
+                    let startPoint = numberCheckPoints[i-1];
+                    let endPoint = numberCheckPoints[i];
+                    if(element.number>startPoint && element.number <= endPoint){
+                        element.image = colors[colorsToUse[colorIndex]];
+                    }
+                    if(colorIndex+1==colorsToUse.length){
+                        colorIndex=0;
+                    }else{
+                        colorIndex++;
+                    }
                 }
             }
             callback(balls);
